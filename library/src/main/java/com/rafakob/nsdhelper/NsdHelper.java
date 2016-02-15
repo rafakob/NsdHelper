ce31d34916e6dedf6d81c7708d90f9c0497ca3cb
Rafal <rafal@tsh.io>
2016-02-15 11:21:53 +0100

    Discovery status getter

--- library/src/main/java/com/rafakob/nsdhelper/NsdHelper.java
+++ library/src/main/java/com/rafakob/nsdhelper/NsdHelper.java
@@ -103,7 +103,7 @@ public class NsdHelper implements DiscoveryTimer.OnTimeoutListener {
 
     /**
      * If no new service has been discovered for a timeout interval, discovering will be stopped
-     * and {@link com.rafakob.nsdmanager.NsdListener#onNsdDiscoveryFinished()} will be called.
+     * and {@link com.rafakob.nsdhelper.NsdListener#onNsdDiscoveryFinished()} will be called.
      * Default timeout is set to 15 seconds.
      * Set 0 to infinite.
      *
@@ -119,6 +119,13 @@ public class NsdHelper implements DiscoveryTimer.OnTimeoutListener {
             mDiscoveryTimeout = seconds;
 
         mDiscoveryTimer.timeout(mDiscoveryTimeout);
+    }
+
+    /**
+     * @return True if discovery is running.
+     */
+    public boolean isDiscoveryRunning() {
+        return mDiscoveryStarted;
     }
 
     /**
@@ -168,7 +175,7 @@ public class NsdHelper implements DiscoveryTimer.OnTimeoutListener {
     /**
      * Start discovering services.
      *
-     * @param serviceType Service type, eg. "_http._tcp.". Typical names can be obtain from {@link com.rafakob.nsdmanager.NsdType}.
+     * @param serviceType Service type, eg. "_http._tcp.". Typical names can be obtain from {@link com.rafakob.nsdhelper.NsdType}.
      */
     public void startDiscovery(String serviceType) {
         startDiscovery(serviceType, null);
@@ -177,7 +184,7 @@ public class NsdHelper implements DiscoveryTimer.OnTimeoutListener {
     /**
      * Start discovering services.
      *
-     * @param serviceType Service type, eg. "_http._tcp.". Typical names can be obtain from {@link com.rafakob.nsdmanager.NsdType}
+     * @param serviceType Service type, eg. "_http._tcp.". Typical names can be obtain from {@link com.rafakob.nsdhelper.NsdType}
      * @param serviceName When service is discovered it will check if it's name contains this text. It is NOT case sensitive.
      */
     public void startDiscovery(String serviceType, String serviceName) {
